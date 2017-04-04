a04d673c37f6e90c21b280691e7015db7acfe830
John Magnusson <johnvhmagnusson@gmail.com>
2017-04-04 16:10:26 +0200

    You can retreive IMEI number from the phone.

--- app/src/main/java/com/bykth/confdroid/confdroid_application/MainActivity.java
+++ app/src/main/java/com/bykth/confdroid/confdroid_application/MainActivity.java
@@ -1,13 +1,51 @@
 package com.bykth.confdroid.confdroid_application;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        final TextView imeiTextView = (TextView) findViewById(R.id.imeiTextView);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
+        {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE))
+            {
+
+            }
+            else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE},1);
+                TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+                final String str = telephonyManager.getDeviceId();
+                imeiTextView.setText(str);
+            }
+        }
+        else
+        {
+            TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+            final String str = "Imei number: " + telephonyManager.getDeviceId();
+            imeiTextView.setText(str);
+        }
+    }
+
 }
