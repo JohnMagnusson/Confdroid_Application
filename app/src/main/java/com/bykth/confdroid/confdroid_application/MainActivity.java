b18f293339f0bea5173ca0b255d3464846eafafe
Elias Axelsson <elle.axelsson@hotmail.com>
2017-04-05 16:26:05 +0200

    Improved request and added button

--- app/src/main/java/com/bykth/confdroid/confdroid_application/MainActivity.java
+++ app/src/main/java/com/bykth/confdroid/confdroid_application/MainActivity.java
@@ -9,14 +9,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    final private int REQUEST_CODE_READ_PHONE_STATE = 1;
+    private TextView imeiTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        System.out.println("Hello");
         setContentView(R.layout.activity_main);
     }
 
@@ -24,20 +30,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart()
     {
         super.onStart();
-        final TextView imeiTextView = (TextView) findViewById(R.id.imeiTextView);
+        imeiTextView = (TextView) findViewById(R.id.imeiTextView);
+        getPermissionToReadPhoneState();
+        Button fetchButton = (Button) findViewById(R.id.button);
 
+        fetchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Thread t = new Thread(new ServerConnection());
+                t.start();
+            }
+        });
+    }
+
+    public void getPermissionToReadPhoneState()
+    {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
         {
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE))
             {
-
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
             }
             else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE},1);
-                TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-                final String str = telephonyManager.getDeviceId();
-                imeiTextView.setText(str);
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
             }
         }
         else
@@ -46,33 +62,27 @@ public class MainActivity extends AppCompatActivity {
             final String str = "Imei number: " + telephonyManager.getDeviceId();
             imeiTextView.setText(str);
         }
-        System.out.println("ABC");
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED)
-        {
-            // Should we show an explanation?
-           /* if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET))
-            {
-
-            }
-            else {*/
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET},3);
-            //}
-        }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED)
-        {
-            // Should we show an explanation?
-           /* if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET))
-            {
-
-            }
-            else {*/
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE},5);
-            //}
-        }
-        System.out.println("DEF");
-        Thread t = new Thread(new ServerConnection());
-        t.start();
-
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        switch(requestCode)
+        {
+            case 1:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+                    final String str = "Imei number: " + telephonyManager.getDeviceId();
+                    imeiTextView.setText(str);
+                }
+                else
+                {
+                    System.out.println("Fail!");
+                }
+
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 }
