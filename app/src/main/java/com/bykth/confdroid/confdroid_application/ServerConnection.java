dd9a1b7b65b46615b79a31b9579159cf44632129
John Magnusson <johnvhmagnusson@gmail.com>
2017-05-23 14:27:38 +0200

    x

--- app/src/main/java/com/bykth/confdroid/confdroid_application/ServerConnection.java
+++ app/src/main/java/com/bykth/confdroid/confdroid_application/ServerConnection.java
@@ -13,65 +13,70 @@ import org.json.JSONObject;
 
 import javax.net.ssl.HttpsURLConnection;
 
-public class ServerConnection
+public class ServerConnection implements Runnable
 {
     public JSONObject retrievedUpdates = null;
-    private String imei = "1234";
+    private String imei;
+    private String hash;
 
-    ServerConnection()
-    {
-        //new Thread(this).start();
-    }
 
     public void fetch(String imei, String versionHash)
     {
         final String imeia = imei;
-        new Thread(){
-            public void run()
-            {
-                synchronized (retrievedUpdates)
-                {
-                    String result = "";
-                    BufferedReader reader = null;
-                    try {
-                        System.out.println("1.1");
-                        URL url = new URL("https://confdroid.tutus.se/api/user.json?userAuth=testToken&imei=" + imeia);
-                        System.out.println("1.2");
-                        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-                        System.out.println("1.3");
-                        InputStream in = new BufferedInputStream(connection.getInputStream());
-                        System.out.println("1.4");
-                        reader = new BufferedReader(new InputStreamReader(in));
-                        System.out.println("1.5");
-                        result = reader.readLine();
-                        System.out.println("I read this: " + result);
-                    }
-                    catch(IOException e)
-                    {
-                        System.out.println("It went wrong: " + e.getMessage());
-                    }
-                    finally
-                    {
-                        if(reader != null) {
-                            try {
-                                reader.close();
-                            } catch (IOException e) {}
-                        }
-                    }
 
+
+
+    }
+
+    public ServerConnection(String imei, String hash)
+    {
+        this.imei = imei;
+        this.hash = hash;
+    }
+
+    public void run()
+    {
+        synchronized (retrievedUpdates)
+        {
+            String result = "";
+            BufferedReader reader = null;
+            try {
+                System.out.println("1.1");
+                URL url = new URL("https://confdroid.tutus.se/api/user.json?userAuth=testToken&imei=" + imeia);
+                System.out.println("1.2");
+                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                System.out.println("1.3");
+                InputStream in = new BufferedInputStream(connection.getInputStream());
+                System.out.println("1.4");
+                reader = new BufferedReader(new InputStreamReader(in));
+                System.out.println("1.5");
+                result = reader.readLine();
+                System.out.println("I read this: " + result);
+            }
+            catch(IOException e)
+            {
+                System.out.println("It went wrong: " + e.getMessage());
+            }
+            finally
+            {
+                if(reader != null) {
                     try {
-                        retrievedUpdates = new JSONObject(result);
-                        retrievedUpdates.notifyAll();
-                        System.out.println("NOOOOOW?: " + retrievedUpdates);
-                        User user = new User(retrievedUpdates.getString("name"));
-                        user.setEmail(retrievedUpdates.getString("email"));
-                        System.out.println(user.getName() + " " + user.getEmail());
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                        reader.close();
+                    } catch (IOException e) {}
                 }
             }
-        }.start();
+
+            try {
+                retrievedUpdates = new JSONObject(result);
+                retrievedUpdates.notifyAll();
+                System.out.println("NOOOOOW?: " + retrievedUpdates);
+                User user = new User(retrievedUpdates.getString("name"));
+                user.setEmail(retrievedUpdates.getString("email"));
+                System.out.println(user.getName() + " " + user.getEmail());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public JSONObject getRetrievedUpdates()
@@ -80,9 +85,5 @@ public class ServerConnection
         return retrievedUpdates;
     }
 
-//    @Override
-//    public void run()
-//    {
-//        fetch("1234", "aa");
-//    }
+
 }
--- app/src/main/java/com/bykth/confdroid/confdroid_application/model/DeviceManagementPolicy.java
+++ app/src/main/java/com/bykth/confdroid/confdroid_application/model/DeviceManagementPolicy.java
@@ -23,5 +23,4 @@ public class DeviceManagementPolicy
     {
         return policy;
     }
-
 }
